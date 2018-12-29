9bf0ceebc005d958177379cb243a348f98a312ca
jero <jero@jero-imedia-S3800>
2018-12-29 19:19:47 +0100

    Prueba y eliminación de basura de fichero backup listado php

--- moveyourlife/forms/lista_actividades_backup.php
+++ moveyourlife/forms/lista_actividades_backup.php
@@ -1,50 +1,14 @@
-<!doctype html>
-<head>
-  <meta charset="utf-8">
-
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <title>Move your life</title>
-  <meta name="description" content="">
-  <meta name="author" content="J. Izquierdo">
-
-  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-
-  <!-- CSS: implied media=all -->
-  <!-- CSS concatenated and minified via ant build script-->
-  <link rel="stylesheet" href="css/style.css">
-  <!-- end CSS-->
-
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
-       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
-       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
-  <script src="js/libs/modernizr-2.0.6.min.js"></script>
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-  <script src="js/mylibs/script.js"></script>
-
-
-</head>
-<body>
-
-
 <?php
 header('Content-Type: text/html; charset=utf-8');
-//Por el momento comento $asignatura. En el futuro será el factor de entrada (CP, Tipo actividad, etc)
+//Por el momento comento $asignatura. En el futuro serï¿½ el factor de entrada (CP, Tipo actividad, etc)
 //$asignatura = $_GET['asignatura'];
 // Create connection
 //echo "<h1>Listado completo para " . $asignatura . "</h1>";
 echo "<h1>Listado completo de actividades </h1>";
-//Datos conexión DDBB
+//Datos conexiï¿½n DDBB
 $servidor = "localhost";
 $usuario = "root";
-$password = "contraseña";
+$password = "contraseï¿½a";
 $tabla = "db750092745";
 //
 $con = new mysqli("localhost", "root", "contraseÃ±a", "problemas");
@@ -119,5 +83,3 @@ echo "</table>";
 
 }
 ?>
-</body>
-</html>
